f68b24adee4800623f3884785d1485338144614c
miriamrebekah <44595296+miriamrebekah@users.noreply.github.com>
2019-10-17 19:34:43 -0400

    working on ab_encrypt()

--- ab_common.c
+++ ab_common.c
@@ -255,7 +255,42 @@ int ab_encrypt(const char *key_file, const char *iv_file, const char *ptext_file
   //	EVP_EncryptInit()
   //	EVP_EncryptUpdate()
   //	EVP_EncryptFinal()
+  PEM_read_bio_PUBKEY();
+  BIO *key_bio = BIO_new_file(key_file, "r");
+  if(!key_bio) goto cleanup; 
+  BIO *iv_bio = BIO_new_file(iv_file, "r");
+  if(!iv_bio) goto cleanup; 
+  BIO *ptext_bio = BIO_new_file(ptext_file, "r");
+  if(!ptext_bio) goto cleanup; 
+  BIO *ctext_bio = BIO_new_file(ctext_file, "w");
+  if(!ctext_bio) goto cleanup; 
+  EVP_CIPHER_CTX *ctx;
+  int len;
+  int ciphertext_len;
 
+  // Create and initialise the context 
+  if(!(ctx = EVP_CIPHER_CTX_new())) goto err;
+
+  // Initialise the encryption operation. IMPORTANT - ensure you use a key
+  // and IV size appropriate for your cipher
+  // In this example we are using 256 bit AES (i.e. a 256 bit key). The
+  // IV size for *most* modes is the same as the block size. For AES this
+  // is 128 bits
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, iv))
+      handleErrors();
+
+  // Provide the message to be encrypted, and obtain the encrypted output.
+  // EVP_EncryptUpdate can be called multiple times if necessary
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+      handleErrors();
+  ciphertext_len = len;
+
+  // Finalise the encryption. Further ciphertext bytes may be written at this stage.
+  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+      handleErrors();
+  ciphertext_len += len;
+  // Clean up
+  EVP_CIPHER_CTX_free(ctx);
   return 0;
 }
 
